ff908e90473883ad149cdbea94c537c613972c8a
samyounger <sam.younger@gmail.com>
2022-05-17 23:40:41 +0100

    jquery upgrade, lunrjs upgrade, search fix

--- assets/javascripts/search.js
+++ assets/javascripts/search.js
@@ -1,44 +1,42 @@
-$(document).ready(function() {
-  // Initialize lunr with the fields to be searched, plus the boost.
-  window.idx = lunr(function () {
-    this.ref('id');
-    this.field('title'), { boost: 10 };
-    this.field('content');
-    this.field('author');
-    this.field('categories');
-    this.field('date');
-
-    this.pipeline.remove(lunr.stemmer)
-    this.pipeline.remove(lunr.stopWordFilter)
-  });
-
+$(document).ready(function () {
   // Get the generated search_data.json file so lunr.js can search it locally.
-  window.data = $.getJSON('/search_data.json');
+  window.data = $.getJSON("../../search_data.json");
 
-  // Wait for the data to load and add it to lunr
-  window.data.then(function(loaded_data){
-    $.each(loaded_data, function(index, value){
-      window.idx.add({
-        id: index,
-        title: value.title,
-        categories: value.categories,
-        content: value.content,
-        author: value.author,
-        date: value.date
-      });
+  window.data.then(data => {
+    // Initialize lunr with the fields to be searched, plus the boost.
+    window.idx = lunr(function () {
+      this.ref("id");
+      this.field("title"), { boost: 10 };
+      this.field("content");
+      this.field("author");
+      this.field("categories");
+      this.field("date");
+
+      // Add the data to lunr
+      Object.keys(data).forEach(key => {
+        // Add the data to lunr
+        this.add({
+          id: key,
+          title: data[key].title,
+          categories: data[key].categories,
+          content: data[key].content,
+          author: data[key].author,
+          date: data[key].date,
+        });
+      }, this);
     });
   });
 
   // // Event when the the input box is changed
-  $(document).on('change keyup', '#search_box', function(event) {
+  $(document).on("change keyup", "#search_box", function (event) {
     let boxValue = $("#search_box").val();
     if (boxValue !== "") {
-      $("#default-post-list").css({ display: "none" })
+      $("#default-post-list").css({ display: "none" });
       let results = window.idx.search(boxValue); // Get lunr to perform a search
       display_search_results(results); // Hand the results off to be displayed
     } else {
       $("#search_results li").remove();
-      $("#default-post-list").css({ display: "unset" })
+      $("#default-post-list").css({ display: "unset" });
     }
   });
 
@@ -46,14 +44,13 @@ $(document).ready(function() {
     const $search_results = $("#search_results");
 
     // Wait for data to load
-    window.data.then(function(loaded_data) {
-
+    window.data.then(function (loaded_data) {
       // Are there any results?
       if (results.length) {
         $search_results.empty(); // Clear any old results
 
         // Iterate over the results
-        results.forEach(function(result) {
+        results.forEach(function (result) {
           let post = loaded_data[result.ref];
 
           // Build a snippet of HTML for this result
@@ -69,7 +66,7 @@ $(document).ready(function() {
         });
       } else {
         // If there are no results, let the user know.
-        $search_results.html('<li>No results found</li>');
+        $search_results.html("<li>No results found</li>");
       }
     });
   }
